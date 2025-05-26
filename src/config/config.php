ef0f6ceeae6337c5652316e25ce981296ae78680
lroux07 <lroux@edenschool.fr>
2025-05-26 14:46:58 +0200

    Update database configuration in config.php to use placeholder name

--- src/config/config.php
+++ src/config/config.php
@@ -5,6 +5,6 @@ define("MODELS", '../src/Models/');
 define("VIEWS", '../src/Views/');
 
 define('HOST', '127.0.0.1');
-define('DATABASE', 'resto-app');
+define('DATABASE', 'DB_NAME');
 define('USER', 'root');
 define('PASSWORD', '');
